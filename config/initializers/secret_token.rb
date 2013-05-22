313938fc21095bc8b305789f73546f3f6745d707
Maurizio Calo Caligaris <mauriziocalo07@gmail.com>
2013-05-22 01:19:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebreakoutchallenge::Application.config.secret_token = '3e284ad8f511d2a5154a83cce7c491b1e3a7b8a28c0f4dd33d914c444d05b549209c6514938885469847ce490cd98f465d348856ee8537087ec46c42d447ff09'
